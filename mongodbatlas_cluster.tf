5750b904d7030f5566a56f9d95a89979111c7db3
Martin Canovas <MartinCanovas@users.noreply.github.com>
2023-09-01 16:44:38 -0700

    Update mongodbatlas_cluster.tf

--- mongodbatlas_cluster.tf
+++ mongodbatlas_cluster.tf
@@ -1,5 +1,5 @@
 resource "mongodbatlas_advanced_cluster" "demo" {
-  project_id   = mongodbatlas_project.demo.id
+  project_id   = var.project_id
   name         = var.cluster_name
   cluster_type = "REPLICASET"
 
@@ -7,6 +7,7 @@ resource "mongodbatlas_advanced_cluster" "demo" {
     region_configs {
       electable_specs {
         instance_size = var.instance_size
+        node_count    = 3
       }
       provider_name         = "AWS"
       region_name           = "US_EAST_2"

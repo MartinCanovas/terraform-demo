d59c67b0f14a423cb265dec3e7698546d3e1be7a
Martin Canovas <MartinCanovas@users.noreply.github.com>
2023-09-01 15:47:31 -0700

    Update mongodbatlas_cluster.tf

--- mongodbatlas_cluster.tf
+++ mongodbatlas_cluster.tf
@@ -1,33 +1,16 @@
-# Create a Cluster
-resource "mongodbatlas_cluster" "tf-support" {
-  project_id         = mongodbatlas_project.tf-support.id
-  name               = var.cluster_name
-  cluster_type       = "REPLICASET"
-  replication_factor = var.replication_factor
-  # cloud_backup                 = true
-  pit_enabled                  = false
-  auto_scaling_disk_gb_enabled = true
-  auto_scaling_compute_enabled = false
-  mongo_db_major_version       = var.mongo_db_major_version
+resource "mongodbatlas_advanced_cluster" "test" {
+  project_id   = mongodbatlas_project.martin.id
+  name         = var.cluster_name
+  cluster_type = "REPLICASET"
 
-  //Provider Settings "block"
-  provider_name = "AWS"
-  disk_size_gb  = 100
-  #provider_disk_iops          = 300
-  provider_volume_type        = "STANDARD"
-  provider_instance_size_name = var.provider_instance_size_name
-  provider_region_name        = var.provider_region_name
-
+  replication_specs {
+    region_configs {
+      electable_specs {
+        instance_size = "M10"
+      }
+      provider_name         = "AWS"
+      region_name           = "US_EAST_2"
+      priority              = 7
+    }
+  }
 }
-
-#resource "mongodbatlas_encryption_at_rest" "test" {
-#  project_id = var.project_id
-
-#  aws_kms = {
-#    enabled                = true
-#    customer_master_key_id = "arn:aws:kms:us-east-2:655848829540:key/f6acd978-40fb-44f8-a190-41634f1a99fb"
-#    region                 = "US_EAST_2"
-#    role_id                = mongodbatlas_cloud_provider_access.role.role_id
-#  }
-
-#}

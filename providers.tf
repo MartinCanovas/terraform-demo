83922cdf5ab5e8842a361d71bddfbfedb79efe34
Martin Canovas <MartinCanovas@users.noreply.github.com>
2023-09-01 15:54:07 -0700

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -2,33 +2,19 @@ terraform {
   required_providers {
     mongodbatlas = {
       source  = "mongodb/mongodbatlas"
-      version = "1.2.0"
-    }
-
-    aws = {
-      source  = "hashicorp/aws"
-      version = ">= 3.0"
+      version = "1.11.0"
     }
   }
-
-  required_version = ">= 0.14"
 }
 
 provider "mongodbatlas" {}
-
-provider "aws" {
-  region     = "us-east-2"
-  profile    = "default"
-  access_key = var.aws-access-key-id
-  secret_key = var.aws-secret-access-key
-}
 
 terraform {
   backend "remote" {
     organization = "Martin_Org"
 
     workspaces {
-      name = "tf-support"
+      name = "demo"
     }
   }
 }

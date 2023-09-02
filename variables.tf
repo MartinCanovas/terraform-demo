92eb574a894facc9298ea8cdb760c5c80e970360
Martin Canovas <martin.canovas@mongodb.com>
2023-09-01 20:02:53 -0700

    M10 tier

--- variables.tf
+++ variables.tf
@@ -13,7 +13,7 @@ variable "mongo_db_major_version" {
 
 variable "instance_size" {
   description = "Instance type"
-  default     = "M40"
+  default     = "M10"
 }
 
 variable "MONGODB_ATLAS_PUBLIC_KEY" {}

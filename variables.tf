f2f2ca1f5a3579d01035a331589cdbccb6810d11
Martin Canovas <martin.canovas@mongodb.com>
2023-09-06 16:05:37 -0700

    Create new cluster M10

--- variables.tf
+++ variables.tf
@@ -3,7 +3,7 @@ variable "project_id" {
 }
 
 variable "cluster_name" {
-  default = "demo"
+  default = "cluster0"
 }
 
 variable "mongo_db_major_version" {
@@ -13,7 +13,7 @@ variable "mongo_db_major_version" {
 
 variable "instance_size" {
   description = "Instance type"
-  default     = "M20"
+  default     = "M10"
 }
 
 variable "MONGODB_ATLAS_PUBLIC_KEY" {}

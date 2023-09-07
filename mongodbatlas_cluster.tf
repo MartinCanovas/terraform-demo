6249477d8e2942858e1f570f370296d22836d6f9
Martin Canovas <martin.canovas@mongodb.com>
2023-09-07 10:57:10 -0700

    Modify MongoDB version to 5.0

--- mongodbatlas_cluster.tf
+++ mongodbatlas_cluster.tf
@@ -2,6 +2,7 @@ resource "mongodbatlas_advanced_cluster" "demo" {
   project_id   = var.project_id
   name         = var.cluster_name
   cluster_type = "REPLICASET"
+  mongo_db_major_version = var.mongo_db_major_version
 
   replication_specs {
     region_configs {
@@ -10,7 +11,7 @@ resource "mongodbatlas_advanced_cluster" "demo" {
         node_count    = 3
       }
       provider_name         = "AWS"
-      region_name           = "US_EAST_1"
+      region_name           = "US_EAST_2"
       priority              = 7
     }
   }

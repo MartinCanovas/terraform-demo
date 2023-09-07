75de08d8d79c838ec80c08ccebcfe5ee536c325f
Martin Canovas <martin.canovas@mongodb.com>
2023-09-07 10:57:10 -0700

    Modify MongoDB version to 5.0

--- variables.tf
+++ variables.tf
@@ -13,7 +13,7 @@ variable "mongo_db_major_version" {
 
 variable "instance_size" {
   description = "Instance type"
-  default     = "M30"
+  default     = "M10"
 }
 
 variable "MONGODB_ATLAS_PUBLIC_KEY" {}

548987d2569740b3165f5b64c8bc03835acaa14e
Martin Canovas <martin.canovas@mongodb.com>
2023-09-22 16:13:52 -0700

    Adding demo db user

---
+++ dbusers.tf
@@ -0,0 +1,11 @@
+resource "mongodbatlas_database_user" "demo" {
+  username           = var.dbuser
+  password           = var.dbuser_pwd
+  project_id         = "622b7a8372fc9a0c13947267"
+  auth_database_name = "admin"
+
+  roles {
+    role_name     = "readWrite"
+    database_name = "admin"
+  }
+}
--- variables.tf
+++ variables.tf
@@ -16,5 +16,15 @@ variable "instance_size" {
   default     = "M01"
 }
 
+variable "dbuser" {
+  description = "Database User Demo"
+  default     = "demo-user"
+}
+
+variable "dbuser_pwd" {
+  description = "Database User Demo Password"
+  default     = "demo-user"
+}
+
 variable "MONGODB_ATLAS_PUBLIC_KEY" {}
 variable "MONGODB_ATLAS_PRIVATE_KEY" {}

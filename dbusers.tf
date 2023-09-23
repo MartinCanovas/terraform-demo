a95af6a47707cfe39cc8ba245e5becd40a962ab8
Martin Canovas <martin.canovas@mongodb.com>
2023-09-23 09:01:19 -0700

    Updating roles for user demo

--- dbusers.tf
+++ dbusers.tf
@@ -4,8 +4,13 @@ resource "mongodbatlas_database_user" "demo" {
   project_id         = "622b7a8372fc9a0c13947267"
   auth_database_name = "admin"
 
+#  roles {
+#    role_name     = "readWrite"
+#    database_name = "admin"
+#  }
+
   roles {
-    role_name     = "readWrite"
+    role_name     = "readWriteAnyDatabase"
     database_name = "admin"
   }
 

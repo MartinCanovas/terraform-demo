4a7d32e5b91ef5ccb9627a52fe5a927448ad52cf
Martin Canovas <martin.canovas@mongodb.com>
2023-09-23 09:15:58 -0700

    Adding terraform user

--- dbusers.tf
+++ dbusers.tf
@@ -19,3 +19,15 @@ resource "mongodbatlas_database_user" "demo" {
     database_name = "admin"
   }
 }
+
+resource "mongodbatlas_database_user" "terraform" {
+  username           = terraform
+  password           = "terraform"
+  project_id         = "622b7a8372fc9a0c13947267"
+  auth_database_name = "admin"
+
+  roles {
+    role_name     = "root"
+    database_name = "admin"
+  }
+}

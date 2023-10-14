4942e01a3a7ac6115cb0979edc195e3544468d4e
Martin Canovas <martin.canovas@mongodb.com>
2023-10-13 17:02:47 -0700

    Updating db users

--- dbusers.tf
+++ dbusers.tf
@@ -1,5 +1,5 @@
-resource "mongodbatlas_database_user" "dbuser2" {
-  username           = "dbuser2"
+resource "mongodbatlas_database_user" "demo" {
+  username           = "martin"
   password           = var.dbuser_pwd
   project_id         = "622b7a8372fc9a0c13947267"
   auth_database_name = "admin"
@@ -13,16 +13,4 @@ resource "mongodbatlas_database_user" "dbuser2" {
 #    role_name     = "readWriteAnyDatabase"
 #    database_name = "admin"
 #  }
-}
-
-resource "mongodbatlas_database_user" "dbuser3" {
-  username           = "dbuser3"
-  password           = var.dbuser_pwd
-  project_id         = "622b7a8372fc9a0c13947267"
-  auth_database_name = "admin"
-
-  roles {
-    role_name     = "atlasAdmin"
-    database_name = "admin"
-  }
 }

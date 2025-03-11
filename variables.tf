cc2fed3fe2830c7cfc18ebb33ef5803094539db5
Martin Canovas <MartinCanovas@users.noreply.github.com>
2025-03-11 09:45:01 -0700

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,21 +1,3 @@
-variable "project_id" {
-  default = "622b7a8372fc9a0c13947267"
-}
-
-variable "cluster_name" {
-  default = "demo"
-}
-
-variable "mongo_db_major_version" {
-  description = "MongoDB version"
-  default     = "6.0"
-}
-
-variable "instance_size" {
-  description = "Instance type"
-  default     = "M10"
-}
-
 variable "dbuser" {
   description = "Database User Demo"
   default     = "demo"
